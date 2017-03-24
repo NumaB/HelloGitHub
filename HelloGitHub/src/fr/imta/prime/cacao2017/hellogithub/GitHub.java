fee37ed100fed6d9a45cff3b6d378c4cf8e8f757
julienbeisel <julienbeisel@gmail.com>
2017-03-24 17:03:13 +0100

    A

---
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/GitHub.java
@@ -0,0 +1,13 @@
+package fr.imta.prime.cacao2017.hellogithub;
+
+
+public class GitHub {
+	
+	public static void AfficherNom (String str){
+		System.out.println(str);
+	}
+	
+	public static void main(String[] args) {
+		AfficherNom("julienbeisel");
+	}
+}
